a42cee4a1e275550404fa482e3ec6cf62115de7f
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-06-28 20:51:08 +0300

    Added script addOneContact.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,24 +1,37 @@
 // import { PATH_DB } from '../constants/contacts.js';
-// import { createFakeContact } from '../utils/createFakeContact.js';
+import { createFakeContact } from '../utils/createFakeContact';
+import path from 'path';
+import fs from 'node:fs/promises';
 
 // const generateContacts = async (number) => {};
 
-// await generateContacts(5);
-import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js';
-import fs from 'node:fs/promises';
-const generateContacts = async (number) => {
+// generateContacts(5);
+
+export const generateContacts = async (numberOfContacts) => {
+  const dbPath = path.resolve(__dirname, '../db/db.json');
+
   try {
-    const data = await fs.readFile(PATH_DB, 'utf-8');
-    const dbData = JSON.parse(data);
-    let newContacts = [];
-    for (let i = 0; i < number; i += 1) {
-      newContacts.push(createFakeContact());
+    // Читання існуючих контактів з файлу
+    let contacts = [];
+    if (
+      await fs
+        .access(dbPath)
+        .then(() => true)
+        .catch(() => false)
+    ) {
+      const data = await fs.readFile(dbPath, 'utf8');
+      contacts = JSON.parse(data);
     }
-    dbData.contacts = dbData.contacts.concat(newContacts);
-    await fs.writeFile(PATH_DB, JSON.stringify(dbData, null, 2), 'utf-8');
+
+    // Створення нових контактів
+    for (let i = 0; i < numberOfContacts; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    // Запис оновлених контактів у файл
+    await fs.writeFile(dbPath, JSON.stringify(contacts, null, 2), 'utf8');
+    console.log('Contacts generated successfully');
   } catch (err) {
-    console.error('Error handling db.json:', err);
+    console.error('Error generating contacts:', err);
   }
 };
-await generateContacts(5);
--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker';
 export const createFakeContact = () => ({
   id: faker.datatype.uuid(), // для генерації унікального ідентифікатора;
   name: faker.person.fullName(), // для генерації повних імен;
-  phone: faker.phone.number(), // для створення телефонних номерів;
+  phone: faker.phone.number('+## (###) ###-##-##'), // для створення телефонних номерів;
   email: faker.internet.email(), // для формування електронних адрес;
   job: faker.person.jobTitle(), // для генерації назв професій.
 });
