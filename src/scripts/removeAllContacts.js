0e09d0d7de8eb5f6fba2319419ba3f6736252127
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-06-30 23:24:20 +0300

    Added unfinished scripts

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([], null, 2), 'utf-8');
+    console.log('Successfully removed all contacts.');
+  } catch (error) {
+    console.error('Error removing contacts:', error);
+  }
+};
 
 await removeAllContacts();
