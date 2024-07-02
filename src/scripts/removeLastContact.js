19a792aab65b89e0ea4cae60e77b1fa1884723d0
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-07-02 20:31:11 +0300

    Update removeLastContact.js

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,16 +5,18 @@ export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-    const updatedContacts = contacts.filter(() => Math.random() >= 0.5);
 
-    await fs.writeFile(
-      PATH_DB,
-      JSON.stringify(updatedContacts, null, 2),
-      'utf-8',
-    );
-    console.log('Successfully!');
+    if (contacts.length === 0) {
+      console.log('The contact array is empty, there is nothing to delete!');
+      return;
+    }
+
+    contacts.pop();
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log('The last contact was successfully deleted!');
   } catch (error) {
-    console.error('Error processing contacts with Thanos:', error);
+    console.error('Error processing contacts:', error);
   }
 };
 
